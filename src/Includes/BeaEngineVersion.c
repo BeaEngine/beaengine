bafb0baf0f15b13aba609c4f4a9f5a3f5adc7405
BeaEngine <beaengine@gmail.com>
2020-11-20 09:56:43 +0100

    version 5.2.0

--- src/Includes/BeaEngineVersion.c
+++ src/Includes/BeaEngineVersion.c
@@ -16,7 +16,7 @@
  *    You should have received a copy of the GNU Lesser General Public License
  *    along with BeaEngine.  If not, see <http://www.gnu.org/licenses/>. */
 const__ char* __bea_callspec__ BeaEngineVersion(void) {
-	return "5.1";
+	return "5.2";
 }
 const__ char* __bea_callspec__ BeaEngineRevision(void) {
 	return "0";
